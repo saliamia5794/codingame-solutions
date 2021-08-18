f958b9868b6490df6cf4b2a9c562fbbee9002022
Amiar <salimamiar@gmail.com>
2021-08-18 03:44:08 +0200

    modification

--- Puzzles/1.Easy/1D Spreadsheet/Solution.java
+++ Puzzles/1.Easy/1D Spreadsheet/Solution.java
@@ -134,7 +134,6 @@ class Solution {
         }
         return result;
     }
-
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
